0e2ce967b665a03ea0b78f73b06189415c9c9d0c
joxinalde <joxinalde@csjn.gov.ar>
2023-11-17 08:57:12 -0300

    ultimo video de Hibernate 65

--- src/main/java/ar/juanprueba/conexionHibernateUnoAVarios/ObtenerPedidosCliente.java
+++ src/main/java/ar/juanprueba/conexionHibernateUnoAVarios/ObtenerPedidosCliente.java
@@ -44,3 +44,39 @@ public class ObtenerPedidosCliente {
 	}
 
 }
+
+/*Si estuvieramos en el fetch Lazy y quisieramos hacer una consulta a la base de datos 
+ * depues de haber cerrado la sesion, debemos usar HQL 
+ 
+try {
+	
+	// comenzamos la transaccion
+	miSession.beginTransaction();
+	
+	// creamos la consulta en lenguaje HQL
+	JOIN FETCH = IGUAL A INNERJOIN, indicamos un parametro con : 
+	 Query<Cliente> consulta = miSession.createQuery("SELECT C FROM cliente C JOIN FETCH C.pedidos WHERE C.id =:elClienteId", Cliente.class);
+	 // le pasamos a la Query el parametro con setParameter, indicando primero el alias que es el parametro(lo que esta despues del : en la consulta) y el valor de ese parametro. 
+	 consulta.setParameter("elClienteId",7)
+	 // cargamos en memoria toda la informacion del cliente que proviene de la consulta
+	 Cliente elCliente = consulta.getSingleResult();
+
+	System.out.printf("Cliente: %s", elCliente);
+
+	
+	// se ejecuta el commit de la transaccion
+	miSession.getTransaction().commit();
+	// cerramos la sesion 
+	miSession.close();
+	
+	System.out.printf("Pedidos: %s", elCliente.getPedidos());
+}catch (Exception e) {
+	e.printStackTrace();
+	
+} finally {
+	
+	// cerramos la sessionFactory
+	miFactory.close();
+}
+
+*/
